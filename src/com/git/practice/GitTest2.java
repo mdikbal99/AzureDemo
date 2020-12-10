914a55132ff8804f7a71fa73456b53b1bdb7c2ee
mdikbal99 <mdtoufiqueikbal@gmail.com>
2020-12-10 15:51:09 +0530

    commit2

--- src/com/git/practice/GitTest2.java
+++ src/com/git/practice/GitTest2.java
@@ -7,16 +7,13 @@ package com.git.practice;
  * @author mdikbal99
  *
  */
-public class GitTest1 {
+public class GitTest2 {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 
-		System.out.println("This is  git tutorial test");
-		
-		System.out.println("This is 2nd commit");
 	}
 
 }
