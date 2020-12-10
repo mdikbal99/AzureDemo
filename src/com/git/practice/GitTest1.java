e43a0017f061b0ec1dcdef2cfeb2f8cd5971a5d5
mdikbal99 <mdtoufiqueikbal@gmail.com>
2020-12-10 15:40:46 +0530

    first commit for GitT

---
+++ src/com/git/practice/GitTest1.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.git.practice;
+
+/**
+ * @author mdikbal99
+ *
+ */
+public class GitTest1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		System.out.println("This is  git tutorial test");
+		
+	}
+
+}
